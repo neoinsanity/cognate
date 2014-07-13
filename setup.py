c74620010e6ec15cb71cddd44f5c4d6acd32e319
neoinsanity <mindbender@gmail.com>
2014-07-13 14:30:55 -0700

    Initial project structure.

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""The setuptools setup file."""
+from setuptools import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    name='cognate',
+    version='0.0.0',
+    author='Raul Gonzalez',
+    author_email='mindbender@gmail.com',
+    url='https://github.com/neoinsanity/cognate',
+    license='Apache License 2.0',
+    description='From the same root',
+    long_description=long_description,
+    packages=['cognate',],
+    install_requires=[],
+    include_package_data = True,
+)
+
