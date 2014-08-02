f58ed2be13bd608edeb28bb068e083a4d80ae4a2
neoinsanity <mindbender@gmail.com>
2014-08-01 17:09:50 -0700

    Fixed a bug where TEST_OUT directory was not being deleted before each test run.

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -16,6 +16,7 @@ find . -type f -iname \*.pyc -delete
 # Remove the current coverage collection file
 rm -f .coverage
 rm -rf BUILD/COVERAGE_REPORT
+rm -rf TEST_OUT
 
 # Execute the tests as per the given config.
 nosetests -c bin/nose.cfg
